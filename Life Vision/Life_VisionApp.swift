59c6d1476eb2b2fad3ef8a347576b421729df569
= <=>
2024-05-24 11:13:24 +0300

    Initial Commit

---
+++ Life Vision/Life_VisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Life_VisionApp.swift
+//  Life Vision
+//
+//  Created by Sinan Dinç on 24.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Life_VisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
