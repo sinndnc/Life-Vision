062ca530c1f6cbed0d2e86cd2f803909202eb655
= <=>
2024-06-08 00:46:02 +0300

    Initial commit

---
+++ Life Vision/Feature/Utils/Entities/SectionItem.swift
@@ -0,0 +1,14 @@
+//
+//  SectionItem.swift
+//  Life Vision
+//
+//  Created by Sinan Dinç on 24.05.2024.
+//
+
+import Foundation
+
+
+struct SectionItem{
+    var label : String
+    var image : String
+}
---
+++ Life Vision/Feature/Utils/Entities/UIState.swift
@@ -0,0 +1,14 @@
+//
+//  UIState.swift
+//  Life Vision
+//
+//  Created by Sinan Dinç on 6.06.2024.
+//
+
+import Foundation
+
+enum UIState{
+    case initial
+    case success
+    case failure
+}
